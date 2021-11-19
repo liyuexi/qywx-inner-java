414e20b0f3c040d10cb478ad47abaaed0ebb2448
lyx <>
2021-11-19 13:57:14 +0800

    小程序登录

--- src/main/java/com/tobdev/qywxinner/controller/XcxController.java
+++ src/main/java/com/tobdev/qywxinner/controller/XcxController.java
@@ -1,12 +1,15 @@
 package com.tobdev.qywxinner.controller;
 
+import com.tobdev.qywxinner.model.entity.QywxInnerUser;
 import com.tobdev.qywxinner.service.QywxInnerService;
+import com.tobdev.qywxinner.utils.JWTUtils;
 import com.tobdev.qywxinner.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -16,7 +19,7 @@ import java.util.Map;
 /**
  * 小程序应用专用
  */
-@Controller
+@RestController
 public class XcxController {
 
     @Autowired
@@ -33,11 +36,25 @@ public class XcxController {
 
 
     @RequestMapping("/xcx/login")
-    @ResponseBody()
-    public Map oauthCallback(HttpServletRequest request,@RequestParam("corp_id") String corpId, @RequestParam("code") String code){
+    public JsonData login(HttpServletRequest request,@RequestParam("corp_id") String corpId, @RequestParam("code") String code){
         //通过code获取信息
         Map result = qywxInnerService.getCode2sessionUser(corpId,code);
-        return  result;
+
+        QywxInnerUser user = new QywxInnerUser();
+        user.setCorpId(corpId);
+        user.setUserId((String) result.get("userid"));
+//        user.setUserId((String) result.get("userid"));
+//        user.setUserType(0);
+//        user.setName((String) result.get("name"));
+//        user.setAvatar((String) result.get("avatar"));
+//        user.setMobile((String) result.get("mobile"));
+//        user.setQrCode((String) result.get("qr_code"));
+        String token=  JWTUtils.geneJsonWebToken(user);
+
+        result.put("token",token);
+
+        return  JsonData.buildSuccess(result);
+
 
     }
 
